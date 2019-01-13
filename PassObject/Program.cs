7307567a60783c3cbef629491acc8f7c8bafefb2
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2019-01-13 20:28:22 +0100

    new and working

--- PassObject/Program.cs
+++ PassObject/Program.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace PassObject
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            Student st1 = new Student();
+
+            Console.WriteLine("eerst heet ze Madeleine");
+            //st1.name = "Madeleine";
+            st1.SetName("Herminione", "Granger"); // call instance method, not static
+            OutputName(st1);
+            Console.Read();
+
+            Console.WriteLine("After being modified");
+            //Setname(st1, "Willa");
+            OutputName(st1);
+            Console.WriteLine("Press enter to terminate");
+            Console.Read();
         }
+        public static void OutputName(Student student)
+        {
+            //Console.WriteLine("Student's name is {0}",student.name);
+            Console.WriteLine("Student zijn/haar naam is "+student.ToNameString());
+        }
+
+        //public static void Setname(Student student, string name)
+        //{
+        //    //student = new Student();
+        //    student.name = name;
+        //    OutputName(student);
+        //}
     }
 }
---
+++ PassObject/Student.cs
@@ -0,0 +1,23 @@
+﻿namespace PassObject
+{
+    // Demonstrate how to pass an object to a method
+     class Student
+    {
+        public string firstName;
+        public string lastName;
+
+        //
+        public void SetName(string fName, string lName)
+        {
+            firstName = fName;
+            lastName = lName; ;
+        }
+
+        //
+        public string ToNameString()
+        {
+            string s = firstName + " " + lastName;
+            return s;
+        }
+    }
+}
